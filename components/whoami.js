6ac3339f4ff5dde98a687dc4440c61cc57aae1b6
Galen Guyer <git@galenguyer.com>
2020-04-21 15:56:41 -0700

    Add description and buttons

--- components/whoami.js
+++ components/whoami.js
@@ -3,7 +3,19 @@ import styles from '../styles/whoami.module.css';
 function WhoAmI() {
     return (
         <div className={styles.container}>
-            
+            {/*<h2 className={styles.name}>Galen Guyer</h2>*/}
+            <span className={styles.description}>
+                2018 Microsoft Office Security, 2019 Microsoft One Customer Voice Intern.
+                <br />
+                Software Engineering Student at the Rochester Institute of Technology. 
+                <br />
+                Full Stack Developer, Systems Administrator, Data Hoarder, and much more.
+            </span>
+
+            <div className={styles.links}>
+            <a href="#" className={styles.button}>Github</a>
+            <a href="#" className={styles.button}>Resume</a>
+            </div>
         </div>
     )
 }
