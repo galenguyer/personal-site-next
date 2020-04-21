674fecf0a0afd7ab9376dd0f228a903878ec98e7
Galen Guyer <git@galenguyer.com>
2020-04-21 15:23:40 -0700

    Add container for ease of width

---
+++ components/whoami.js
@@ -0,0 +1,11 @@
+import styles from '../styles/whoami.module.css';
+
+function WhoAmI() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
+
+export default WhoAmI;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Header from '../components/header';
+import WhoAmI from '../components/whoami';
 
 export default () => (
-  <>
+  <div id='container'>
     <Header />
-  </>
+    <WhoAmI />
+  </div>
 )
