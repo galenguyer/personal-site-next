7dd94ca7af4e36ef8dffc59cb72f1609033f49ed
Galen Guyer <git@galenguyer.com>
2020-04-21 13:37:09 -0700

    Implement smart navbar

--- components/navlink.js
+++ components/navlink.js
@@ -3,12 +3,12 @@ import styles from '../styles/nav.module.css';
 
 function NavLink({ children, href }) {
   const router = useRouter()
-  const style = {
-    marginRight: 10,
+  let style = router.pathname === href ? {
     background: '#3a444a',
-    borderTopColor: '#61a8df',
+    borderTop: '4px solid #61a8df',
 	color: '#fff',
-  }
+  } : {}
+
 
   const handleClick = e => {
     e.preventDefault()
@@ -16,7 +16,7 @@ function NavLink({ children, href }) {
   }
 
   return (
-      <li className={styles.item}>
+      <li style={style} className={styles.item}>
         <a href={href} onClick={handleClick} className={styles.link}>
             {children}
         </a>
