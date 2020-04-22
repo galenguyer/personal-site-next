02b7f6589df0f08ca4a603016aee80dd8866b18a
Galen Guyer <git@galenguyer.com>
2020-04-21 17:35:12 -0700

    make name at top link to home

--- components/logo.js
+++ components/logo.js
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import styles from '../styles/logo.module.css';
  
 function Logo() {
     return (
         <div className={styles.logo}>
             <div className={styles.bars}>
-                <h1 className={styles.name}>Galen Guyer</h1>
+                <Link href='/'><h1 className={styles.name}>Galen Guyer</h1></Link>
                 <span className={styles.description}>Full-Stack Software Engineer studying at the Rochester Institute of Technology</span>
             </div>
         </div>
